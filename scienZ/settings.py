be369c6140bd6685a06b1918657724ddee01f11b
vlcode110 <84029989+vlcode110@users.noreply.github.com>
2021-12-03 00:46:08 +0000

    Update settings.py

--- scienZ/settings.py
+++ scienZ/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ja7^#589+)o@^!v_$4s=^+rs1g31tei_v63z_tp$ron=$e9yw#
 DEBUG = os.getenv("DEBUG", "False") == "True"
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", *).split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", 'http://0.0.0.0:8080').split(",")
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # Application definition
