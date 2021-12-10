4d71960a4a8099f5d87f8f1ac687907ccd12b502
vlcode110 <84029989+vlcode110@users.noreply.github.com>
2021-12-10 01:57:31 +0000

    Update settings.py

--- scienZ/settings.py
+++ scienZ/settings.py
@@ -15,6 +15,7 @@ import os
 import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
+from .cdn.conf import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
