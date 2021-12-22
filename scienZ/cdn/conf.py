61ba2ebfef015bcbf2ea402a29f3b9a4e831f0fa
vlcode110 <84029989+vlcode110@users.noreply.github.com>
2021-12-22 18:14:56 +0000

    Update conf.py

--- scienZ/cdn/conf.py
+++ scienZ/cdn/conf.py
@@ -2,7 +2,7 @@ import os
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = "scien-z"
-AWS_S3_ENDPOINT_URL = "https://scien-z.ams3.digitaloceanspaces.com"
+AWS_S3_ENDPOINT_URL = "https://scien-z-ul2t7.ondigitalocean.app"
 AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=86400",
 }
