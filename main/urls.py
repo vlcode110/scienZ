ef8b4ab34876a48841e51d8ab754897e15d828ad
vlcode110 <84029989+vlcode110@users.noreply.github.com>
2021-12-23 19:38:24 +0000

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 app_name = 'main'
 
