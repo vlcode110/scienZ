8242d2bd424eddb6d0047cca767f651f0aa2f1fa
vlcode110 <84029989+vlcode110@users.noreply.github.com>
2021-12-23 19:37:06 +0000

    Update urls.py

--- scienZ/urls.py
+++ scienZ/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 
 
